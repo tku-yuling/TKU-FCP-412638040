e487e906d1f181a6ad11da419a34407996de8332
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-02 13:29:01 +0800

    Add files via upload

---
+++ ch2-1.java
@@ -0,0 +1,12 @@
+//ch2-1, 簡單的java程式
+public class Ch2-1 {  //定義public類別ch2_1
+    public static void main(String[] args){  //main()函數，主程式開始
+        int num=2;  //宣告整數num
+        int nym=3;
+        float num3=4;
+        String 
+        System.out.println(num+"cats are running"); // 印出字串及變數內容
+        System.out.println(num2+"dogs are running");
+
+    }
+}
---
+++ simple.java
@@ -0,0 +1,10 @@
+/**
+ * 0702
+ */
+public class simple {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("0702");
+    }
+    
+}
