650f889917ceebadc3bb00f6d310cadf9a635340
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-03 11:45:23 +0800

    Update ch2_6.java

--- ch2_6.java
+++ ch2_6.java
@@ -1,8 +1,8 @@
 public class ch2_6 {   //定義類別 ch2_6
-    public static void main(String[] args) {
+    public static void main(String[] args) {   // main()函數，主程式開始 
         int n1=12;    
         int n2=26;    
-        System.out.println(n1+"+"+n2+"="+(n1+n2));
+        System.out.println(n1+"+"+n2+"="+(n1+n2)); // 印出n1+n2之值
         System.out.println("n1+n2="+(n1+n2) );
         System.out.println((n1+n2));
     }
