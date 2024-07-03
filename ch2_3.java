bd101ca3782f204167c793df9e68073e5ca34830
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-03 11:15:42 +0800

    Add files via upload

---
+++ ch2_3.java
@@ -0,0 +1,7 @@
+public class ch2_3 {   //定義類別 ch2_3
+    public static void main(String[] args) {
+        int a=2, b=5;   //宣告變數a和b
+
+        System.out.println(a+"*"+b+"="+(a*b));  //印出運算結果
+    }
+}
