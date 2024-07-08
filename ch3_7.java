6cca3074514f75b9b8f83f440823e44535f88782
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-08 11:44:13 +0800

    add

---
+++ ch3_7.java
@@ -0,0 +1,8 @@
+public class ch3_7 {
+    
+    public static void main(String[] args) {
+        float n=5.0f;
+        double p=n*n;
+        System.out.println(n+"*"+n+"="+p);
+    }
+}
---
+++ ch3_8.java
@@ -0,0 +1,9 @@
+public class ch3_8 {
+    
+    public static void main(String[] args) {
+        System.out.println("f_max="+Float.MAX_VALUE);
+        System.out.println("f_min="+Float.MIN_VALUE);
+        System.out.println("d_max="+Double.MAX_VALUE);
+        System.out.println("f_max="+Double.MIN_VALUE);
+    }
+}
