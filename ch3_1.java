ce02231ef6ec07cc7a3d0d86c714846ccc8de52a
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-08 11:08:37 +0800

    add

---
+++ ch3_1.java
@@ -0,0 +1,11 @@
+public class ch3_1 {
+
+    public static void main(String[] args) {
+        int num=3;
+        char ch='C';
+        System.out.println((num+" is ab integer"));
+        System.out.println((ch+" is ab cgaracter"));
+
+
+    }
+}
---
+++ ch3_3.java
@@ -0,0 +1,14 @@
+public class ch3_3 {
+    
+    public static void main(String[] args) {
+        byte bin =0b1111;             //以二進位表示的整數
+        short oct=02001;              //以八進位表示的整數
+        int hex1 =0xff12;             //以十六進位表示的整數
+        long hex2=0x12345ffeL;        //以十六進位表示的整數
+
+        System.out.println("bin= " +bin);
+        System.out.println("oct= " +oct);
+        System.out.println("hex1= " +hex1);
+        System.out.println("hex2= " +hex2);
+    }
+}
