b2cadc68a1d9a051993c41b43ec7716ee102287e
tku-yuling <412638040@o365.tku.edu.tw>
2024-07-16 12:00:26 +0800

    add

---
+++ ch6_1.java
@@ -0,0 +1,15 @@
+public class ch6_1 {
+
+    public static void main(String[] args) {
+        int[] a=new int[4];
+        a[0]=9;
+        a[1]=6;
+        a[2]=5;
+        a[3]=7;
+
+        for(int i=0; i<a.length; i++)
+        System.out.printf("a[%d]=%d,,",i,a[i]);
+        System.out.printf("length", a.length);
+
+    }
+}
